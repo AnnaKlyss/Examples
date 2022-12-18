e189f901d04abe1d96c9a196aad9fa841d798313
Anna <anya-suenkova@yandex.ru>
2022-12-18 18:13:31 +0300

    add example011

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // index = index +1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Index0f(int[] collection, int find)
+{
+   int count = collection.Length;
+   int index = 0;
+   int position = -1; // если может быть ситуация, что искомого элемента нет совсем.
+
+   while (index < count)
+   {
+        if (collection[index] == find)
+        {
+               position = index; // покажет последнее вхождение в массив заданного числа
+               break; //если добавить эту строку - покажет первое вхождение
+        }
+        index++;
+   }
+
+    return position;
+}
+
+int[] array = new int[10];  //создай новый массив, в котором будет 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Index0f(array, 4);
+Console.WriteLine(pos);
+
